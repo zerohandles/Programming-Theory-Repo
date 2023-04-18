3c5f6e77ffea7c0bf0d25ada5f95c9957eff9471
zerohandles <103554676+zerohandles@users.noreply.github.com>
2023-04-18 14:03:04 -0500

    Character selection added
    added the character selection UI elements to the title screen.  Imported TMP components.

---
+++ Assets/CharacterSelect.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterSelect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void SelectPunk()
+    {
+        Debug.Log("Punk Selected");
+    }
+
+    public void SelectLawyer()
+    {
+        Debug.Log("Lawyer Selected");
+    }
+
+    public void SelectFarmer()
+    {
+        Debug.Log("Farmer Selected");
+    }
+
+}
