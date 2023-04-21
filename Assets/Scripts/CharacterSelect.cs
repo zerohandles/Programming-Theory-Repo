3d34e202bd6d7c8df24be1858e1e8cd1349179e6
zerohandles <103554676+zerohandles@users.noreply.github.com>
2023-04-21 13:08:14 -0500

    Added character select and scene change
    when the player selects a character from the start screen the main scene will load. Added help text for each character class.

--- Assets/Scripts/CharacterSelect.cs
+++ Assets/Scripts/CharacterSelect.cs
@@ -1,33 +1,40 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CharacterSelect : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public int playerChoice = 0;
+
+
     void Start()
     {
-        
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (playerChoice != 0)
+        {
+            SceneManager.LoadScene(1);
+        }
     }
 
     public void SelectPunk()
     {
+        playerChoice = 2;
         Debug.Log("Punk Selected");
     }
 
     public void SelectLawyer()
     {
+        playerChoice = 1;
         Debug.Log("Lawyer Selected");
     }
 
     public void SelectFarmer()
     {
+        playerChoice = 3;
         Debug.Log("Farmer Selected");
     }
 
